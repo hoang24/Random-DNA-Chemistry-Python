da2aabe4ba571c6e576de153c20e7540af804461
Hoang Nguyen <hoang24@pdx.edu>
2020-02-28 19:25:05 -0800

    fix simulate chem code to be able to work with multiple trajectories

--- core/simulate_perturbed_chem.py
+++ core/simulate_perturbed_chem.py
@@ -20,7 +20,7 @@ plt.xlabel('Time (s)')
 plt.ylabel('Number of molecules')
 
 gillespy2_results = []
-num_trajectories = 1
+num_trajectories = 5
 
 # Creating the Gillespy2 chemistry model in the non-perturb period
 gillespy2_model = RandomDNAChemPerturbationGillespy2(non_gillespy2_chem=randomDNAChem,
@@ -49,15 +49,19 @@ for time_index in range(1, len(randomDNAChem.time_params['time_array']) - 1):
     # Calculate the Gillespy2 time offset
     time_offset = randomDNAChem.time_params['t_perturb'] + randomDNAChem.time_params['t_hold'] * (time_index - 1)
 
-    # Creating one Gillespy2 chemistry model in one perturb period
-    gillespy2_model = RandomDNAChemPerturbationGillespy2(non_gillespy2_chem=randomDNAChem,
-                                                         rate_in_timeIndex=time_index, # index 1 for time t=1 and so on
-                                                         period_start=randomDNAChem.time_params['time_array'][time_index] - time_offset,
-                                                         period_end=randomDNAChem.time_params['time_array'][time_index + 1] - time_offset,
-                                                         previous_gillespy2_result=gillespy2_result)
+    trajectories = []
+    for index in range(num_trajectories):
+        previous_trajectory = gillespy2_result[index] # take the gillespy2_result from the previous period
 
-    # Result of stochastic Gillespie simulation for one perturbation period
-    gillespy2_result = gillespy2_model.run(number_of_trajectories=num_trajectories)
+        # Creating one Gillespy2 chemistry model in one trajectory in one perturb period
+        gillespy2_model = RandomDNAChemPerturbationGillespy2(non_gillespy2_chem=randomDNAChem,
+                                                             rate_in_timeIndex=time_index, # index 1 for time t=1 and so on
+                                                             period_start=randomDNAChem.time_params['time_array'][time_index] - time_offset,
+                                                             period_end=randomDNAChem.time_params['time_array'][time_index + 1] - time_offset,
+                                                             previous_gillespy2_result=previous_trajectory)
+        trajectory = gillespy2_model.run(number_of_trajectories=1) # run the single trajectory
+        trajectories.append(trajectory)
+    gillespy2_result = trajectories # gillespy2_result of the current period
 
     # Result of stochastic Gillespie simulation for entire simulation time
     gillespy2_results.append(gillespy2_result)
@@ -74,10 +78,12 @@ for time_index in range(1, len(randomDNAChem.time_params['time_array']) - 1):
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = OrderedDict(zip(labels, handles))
 plt.legend(by_label.values(), by_label.keys(), loc='best')
-# plot_name = 'random_dna_chem_with_perturb'
+# plot_name = 'random_dna_chem_with_perturb_junk'
 try:
     plot_name
 except NameError:
     plt.show()
 else:
     plt.savefig('plots/' + plot_name + '.eps')
+
+# print(gillespy2_results)
