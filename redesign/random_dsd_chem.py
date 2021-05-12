4995b24d4e9ae13af51b87ac55a40490da612aa3
Hoang Nguyen <hoang24@pdx.edu>
2021-05-11 23:16:27 -0700

    methods to generate visualDSD script

--- redesign/random_dsd_chem.py
+++ redesign/random_dsd_chem.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+from string import Template
 
 
 class RandomDSDChemistry():
@@ -8,15 +9,29 @@ class RandomDSDChemistry():
         Generate Visual DSD Script
     '''
 
-    def __init__(self, input_params, time_params):
+    def __init__(self, input_params, time_params, dsd_filename, initial, final):
         '''
             Python initialization method.
             Args:
                 input_params (dict): input parameters
                 time_params (dict): timing parameters
+                dsd_filename (str): location and name of the rendered Visual DSD file
+                initial (float): start time of the simulation period
+                final (float): end time of the simulation period
         '''
+
+        # Constant parameters
+        self.bind_rate = 0.003 # base value of binding rate
+        self.unbind_rate = 0.1 # base value of unbinding rate
+        self.points = 1001 # number of datapoints in the simulation period
+
+        # User-defined parameters
         self.input_params = input_params
         self.time_params = time_params
+        self.dsd_filename = dsd_filename
+        self.initial = initial
+        self.final = final
+
         self.create_species_single()
         self.create_domain_lookup()
         self.create_species_double()
@@ -38,6 +53,13 @@ class RandomDSDChemistry():
         self.create_reaction_sets()
         self.create_reaction_lookup()
         self.create_rateConst_lookup()
+        self.render_species()
+        self.render_domains()
+        self.render_species_single()
+        self.render_species_double()
+        self.render_initial_conditions()
+        self.render_reactions()
+        self.render_DSD_program()
 
     def create_species_single(self):
         '''
@@ -59,19 +81,26 @@ class RandomDSDChemistry():
 
     def create_domain_lookup(self):
         '''
-            Method to assign the switching domain types 
+            Method to assign the switching domain types
+            switching_domains (list of str): list of switching domain types (e.g. A, B, C, etc.)
+            identity_domains (list of str): list of identity domain types (e.g. 1, 2, 3, 4, etc.)
         '''
         SWITCHING_DOMAINS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        switching_domains = list(SWITCHING_DOMAINS[:self.input_params['s']])
+        self.switching_domains = list(SWITCHING_DOMAINS[:self.input_params['s']])
+
+        if self.nU >= self.nL:
+            self.identity_domains = [i[1:len(i)] for i in self.U]
+        else:
+            self.identity_domains = [i[1:len(i)] for i in self.L]
 
         self.domain_lookup = {'U': {}, 'L': {}}
         for u in self.U:
-            switching_domain = str(np.random.choice(switching_domains))
+            switching_domain = str(np.random.choice(self.switching_domains))
             self.domain_lookup['U'].update({u: ['M', 'M', switching_domain, u[-1]]})
 
         for l in self.L:
-            switching_domain = str(np.random.choice(switching_domains))
-            self.domain_lookup['L'].update({l: ['M*', 'M*', switching_domain + '*', l[-1] + '*']})
+            switching_domain = str(np.random.choice(self.switching_domains))
+            self.domain_lookup['L'].update({l: ['M', 'M', switching_domain, l[-1]]})
 
     def create_species_double(self):
         '''
@@ -88,8 +117,8 @@ class RandomDSDChemistry():
         self.PW = []
         for u in self.U:
             for l in self.L:
-                if self.domain_lookup['U'][u][2] == self.domain_lookup['L'][l][2][:-1]: # check if switching domains are the same type for upper and lower strands (exclude the *)
-                    if self.domain_lookup['U'][u][3] == self.domain_lookup['L'][l][3][:-1]: # check if identity domains are the same type for upper and lower strands (exclude the *)
+                if self.domain_lookup['U'][u][2] == self.domain_lookup['L'][l][2]: # check if switching domains are the same type for upper and lower strands
+                    if self.domain_lookup['U'][u][3] == self.domain_lookup['L'][l][3]: # check if identity domains are the same type for upper and lower strands
                         self.F.append(u + l)
                     else:
                         self.PS.append(u + l)
@@ -279,7 +308,7 @@ class RandomDSDChemistry():
             for ps in self.PS: # for each strong partial double strand
                 du, dl = self.decode_double(ps)
                 if u not in ps: # if single strand is not part of double strand
-                    if (self.domain_lookup['U'][u][2] == self.domain_lookup['L'][dl][2][:-1]) and (self.domain_lookup['U'][u][3] == self.domain_lookup['L'][dl][3][:-1]): # if the switching and identity domain of the upper strand matches that of the lower part of the double strand
+                    if (self.domain_lookup['U'][u][2] == self.domain_lookup['L'][dl][2]) and (self.domain_lookup['U'][u][3] == self.domain_lookup['L'][dl][3]): # if the switching and identity domain of the upper strand matches that of the lower part of the double strand
                         prod = u + dl
                         R_displace = f"{u}() + {ps}() ->{{}} {du}() + {prod}()"
                         self.R_DISPLACE.append(R_displace)
@@ -287,7 +316,7 @@ class RandomDSDChemistry():
             for pw in self.PW: # for each weak partial double strand
                 du, dl = self.decode_double(pw)
                 if u not in pw: # if single strand is not part of double strand
-                    if self.domain_lookup['U'][u][2] == self.domain_lookup['L'][dl][2][:-1]: # if the switching domain of the upper strand matches that of the lower part of the double strand
+                    if self.domain_lookup['U'][u][2] == self.domain_lookup['L'][dl][2]: # if the switching domain of the upper strand matches that of the lower part of the double strand
                         prod = u + dl
                         R_displace = f"{u}() + {pw}() ->{{}} {du}() + {prod}()"
                         self.R_DISPLACE.append(R_displace)
@@ -295,16 +324,16 @@ class RandomDSDChemistry():
         for l in self.L: # for each lower strand
             for ps in self.PS: # for each strong partial double strand
                 du, dl = self.decode_double(ps)
-                if u not in ps: # if single strand is not part of double strand
-                    if (self.domain_lookup['U'][du][2] == self.domain_lookup['L'][l][2][:-1]) and (self.domain_lookup['U'][u][3] == self.domain_lookup['L'][dl][3][:-1]): # if the switching and identity domain of the upper strand matches that of the lower part of the double strand
+                if l not in ps: # if single strand is not part of double strand
+                    if (self.domain_lookup['U'][du][2] == self.domain_lookup['L'][l][2]) and (self.domain_lookup['U'][du][3] == self.domain_lookup['L'][l][3]): # if the switching and identity domain of the upper strand matches that of the lower part of the double strand
                         prod = du + l
                         R_displace = f"{l}() + {ps}() ->{{}} {dl}() + {prod}()"
                         self.R_DISPLACE.append(R_displace)
 
             for pw in self.PW: # for each weak partial double strand
                 du, dl = self.decode_double(pw)
-                if u not in pw: # if single strand is not part of double strand
-                    if self.domain_lookup['U'][du][2] == self.domain_lookup['L'][l][2][:-1]: # if the switching domain of the lower strand matches that of the upper part of the double strand
+                if l not in pw: # if single strand is not part of double strand
+                    if self.domain_lookup['U'][du][2] == self.domain_lookup['L'][l][2]: # if the switching domain of the lower strand matches that of the upper part of the double strand
                         prod = du + l
                         R_displace = f"{l}() + {pw}() ->{{}} {dl}() + {prod}()"
                         self.R_DISPLACE.append(R_displace)
@@ -398,7 +427,7 @@ class RandomDSDChemistry():
             Methods to create a lookup dictionary for sets of reactions and number of reactions
             reaction_lookup (dict): lookup dictionary for sets of reactions and number of reactions
         '''
-        reaction_lookup = {
+        self.reaction_lookup = {
             'R_BIND': self.R_BIND,
             'R_DISPLACE': self.R_DISPLACE,
             'R_IN': self.R_IN,
@@ -440,10 +469,142 @@ class RandomDSDChemistry():
             'rate_OUT': self.rate_OUT
         }
 
+    def render_species(self):
+        '''
+            Render the species from Random DNA Strand Circuit species
+            Args:
+                species (tuple of str): all species in the Random DNA Strand Circuit
+            Returns:
+                rendered_species (list of str): rendered visualDSD species in the S() format
+        '''
+        self.rendered_species = []
+        for s in self.S:
+            self.rendered_species.append(f'{s}()')
+
+    def render_domains(self):
+        '''
+            Render the Domains definition 'dom' in the Visual DSD code
+            rendered_domains (list of str): list of domain definitions for each species (dom t0 ..., dom t1..., etc.)
+        '''
+        self.rendered_domains = []
+
+        self.rendered_domains.append(f'dom M = {{bind={self.bind_rate}; unbind={self.unbind_rate}; colour="black"}} // main domain')
+
+        for switching_domain in self.switching_domains:
+            self.rendered_domains.append(f'dom {switching_domain} = {{bind={self.bind_rate}; unbind={self.unbind_rate}}} // switching domain')
+
+        for identity_domain in self.identity_domains:
+            self.rendered_domains.append(f'dom t{identity_domain} = {{bind={self.bind_rate}; unbind={self.unbind_rate}}} // identity domain')
+
+    def render_species_single(self):
+        '''
+            Render the Modules definition 'def' for the single strands (upper and lower) in the Visual DSD code
+            rendered_single (list of str): list of upper and lower strand modules
+        '''
+        self.rendered_single = []
+        for u in self.U:
+            doms = self.domain_lookup['U'][u]
+            self.rendered_single.append(f'def {u}() = <{doms[0]}^ {doms[1]}^ {doms[2]}^ t{doms[3]}^> // upper strand')
+
+        for l in self.L:        
+            doms = self.domain_lookup['L'][l]
+            self.rendered_single.append(f'def {l}() = {{{doms[0]}^* {doms[1]}^* {doms[2]}^* t{doms[3]}^*}} // lower strand')
+
+    def render_species_double(self):
+        '''
+            Render the Modules definition 'def' for the double strands (full and partial) in the Visual DSD code
+            rendered_doubles (list of str): list of partial and full double strand modules
+            rendered_singles (list of str): list of upper and lower strand modules
+        '''
+        self.rendered_double = []
+        for f in self.F:
+            u, l = self.decode_double(f)
+            doms_u = self.domain_lookup['U'][u]
+            doms_l = self.domain_lookup['L'][l]
+            if doms_u != doms_l:
+                raise Exception(f'{f} is not a full double strand')
+            self.rendered_double.append(f'def {f}() = [{doms_u[0]}^ {doms_u[1]}^ {doms_u[2]}^ t{doms_u[3]}^] // full double strand')
+
+        for ps in self.PS:
+            u, l = self.decode_double(ps)
+            doms_u = self.domain_lookup['U'][u]
+            doms_l = self.domain_lookup['L'][l]
+            if doms_u[2] != doms_l[2]:
+                raise Exception(f'{ps} is not a strong partial double strand')
+            self.rendered_double.append(f'def {ps}() = [{doms_u[0]}^ {doms_u[1]}^ {doms_u[2]}^]<t{doms_u[3]}^>{{t{doms_l[3]}^*}} // strong (2/3) partial double strand')
+
+        for pw in self.PW:
+            u, l = self.decode_double(pw)
+            doms_u = self.domain_lookup['U'][u]
+            doms_l = self.domain_lookup['L'][l]
+            self.rendered_double.append(f'def {pw}() = [{doms_u[0]}^ {doms_u[1]}^]<{doms_u[2]}^ t{doms_u[3]}^>{{{doms_l[2]}^* t{doms_l[3]}^*}} // weak (1/3) partial double strand')
+
+    def render_initial_conditions(self):
+        '''
+            Render the Initial Conditions of the Visual DSD Code
+            rendered_initial_conditions (list of str): list of the rendered DSD initial conditions
+        '''
+        conU = self.species_count_lookup['conU']
+        conL = self.species_count_lookup['conL']
+        conF = self.species_count_lookup['conF']
+        conPS = self.species_count_lookup['conPS']
+        conPW = self.species_count_lookup['conPW']
+
+        self.rendered_initial_conditions = []
+        for u, conu in conU.items():
+            self.rendered_initial_conditions.append(f'{conu[0]} {u}()')
+        for l, conl in conL.items():
+            self.rendered_initial_conditions.append(f'{conl[0]} {l}()')
+        for f, conf in conF.items():
+            self.rendered_initial_conditions.append(f'{conf[0]} {f}()')
+        for ps, conps in conPS.items():
+            self.rendered_initial_conditions.append(f'{conps[0]} {ps}()')
+        for pw, conpw in conPW.items():
+            self.rendered_initial_conditions.append(f'{conpw[0]} {pw}()')
+
+    def render_reactions(self):
+        '''
+            Render the Visual DSD reactions and rates.
+            rendered_reactions (list of str): list of rendered DSD reactions and rates
+        '''
+        self.rendered_reactions = []
+        for R_bind in self.R_BIND:
+            self.rendered_reactions.append(R_bind + ' // binding reaction')
+        for R_displace in self.R_DISPLACE:
+            self.rendered_reactions.append(R_displace + ' // displacement reaction')
+        for R_in in self.R_IN:
+            self.rendered_reactions.append(R_in + ' // influx reaction')
+        for R_out in self.R_OUT:
+            self.rendered_reactions.append(R_out + ' // efflux reaction')
+
+    def render_DSD_program(self):
+        '''
+            Render the visual DSD program
+        '''
+        self.rendered_modules = self.rendered_single + self.rendered_double
+        data_DSD = {
+            'initial': self.initial,
+            'final': self.final,
+            'points': self.points,
+            'species_list': '; '.join(self.rendered_species),
+            'dom_list': '\n'.join(self.rendered_domains),
+            'def_list': '\n'.join(self.rendered_modules),
+            'initial_cond_list': '\n| '.join(self.rendered_initial_conditions),
+            'reaction_list': '\n| '.join(self.rendered_reactions)
+        }
+
+        with open('dsd_template.txt', 'r') as template_file:
+            template_DSD = Template(template_file.read())
+        self.rendered_DSD = template_DSD.substitute(data_DSD)
+
+        with open(self.dsd_filename, 'w') as rendered_file:
+            rendered_file.write(self.rendered_DSD)
 
 if __name__ == '__main__':
     from params import input_params, time_params
     import code
 
-    randDSDChem = RandomDSDChemistry(input_params, time_params)
-    code.interact(local=locals())
+    dsd = RandomDSDChemistry(input_params=input_params, time_params=time_params, dsd_filename='dsd_rendered.txt', initial=0.00, final=0.01, )
+    # for r in dsd.rendered_double:
+    #     print(r)
+    # code.interact(local=locals())
